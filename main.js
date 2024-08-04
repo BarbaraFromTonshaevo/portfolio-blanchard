796ba37daeda03866af98459567ec51226b8aecd
Elkina Barbara <elkinavarvara1999@gmail.com>
2024-08-04 21:26:01 +0300

    desktop sections

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 // import './src/js/slider';
+import Swiper from "swiper";
+const gallery = document.querySelector('.gallery__slider');
+if(gallery){
+    const swiper = new Swiper(gallery, {
+			slidesPerView: 3,
+            spaceBetween: 50,
+		})
+}
